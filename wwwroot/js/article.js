a1fbdccf2410d324d19cc7d5eb858389c69d8698
christian dungca <cdungca14@gmail.com>
2019-09-25 21:29:41 -0700

    tired at jordie's house

--- wwwroot/js/article.js
+++ wwwroot/js/article.js
@@ -143,40 +143,31 @@ housingExplore.addEventListener('click', () => {
 // //////////////////////// //
 
 function smoothScroll(target, duration){
-    var target = document.querySelector(target);
-    // gives the targets left position based off of the window
-    console.log("target is : " + target);
+    var target = document.querySelector(target)
     var targetPosition = target.getBoundingClientRect().left;
-    console.log("target's position : " + targetPosition);
-    // the window
     var startPosition = window.pageXOffset;
-    console.log("starting position : " + startPosition);
-
     var distance = targetPosition - startPosition;
-    console.log("distance between target pos. and start pos. : " + distance);
-
     var startTime = null;
 
     function animation(currentTime){
         if(startTime === null) startTime = currentTime;
         var timeElapsed = currentTime - startTime;
         var run = ease(timeElapsed,startPosition, distance, duration);
-        window.scrollTo(run, 0);
+        window.scrollTo(run);
         if(timeElapsed < duration) requestAnimationFrame(animation);
-        window.scrollTo(run, 0); 
         console.log(`the run function equals ${run}`);
     }
 
-    function ease(t,b,c,d){
-        t /= d /2;
-        if (t < 1) return c / 2 * t * t + b;
+    Math.ease = function (t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
         t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
+        return -c/2 * (t*(t-2) - 1) + b;
     }
 
     requestAnimationFrame(animation);
-    console.log(targetPosition);
 }
+
 
 // //////////////////////// //
 //    selecting elements    //
@@ -191,21 +182,35 @@ var sectionSix = document.querySelector('.sectionSix');
 
 // calling the functions
 sectionOne.addEventListener('click', function(){
-    smoothScroll('.one', 1000);
-})
-sectionTwo.addEventListener('click', function(){
-    smoothScroll('.two', 1000);
-})
-sectionThree.addEventListener('click', function(){
-    smoothScroll('.three', 1000);
-})
-sectionFour.addEventListener('click', function(){
     smoothScroll('.four', 1000);
-})
-sectionFive.addEventListener('click', function(){
-    smoothScroll('.five', 1000);
-})
-sectionSix.addEventListener('click', function(){
-    smoothScroll('.six', 1000);
-})
+});
+     
+ function scrollTo(element, to, duration) {
+     var start = element.scrollTop,
+         change = to - start,
+         currentTime = 0,
+         increment = 20;
+         
+     var animateScroll = function(){        
+         currentTime += increment;
+         var val = Math.easeInOutQuad(currentTime, start, change, duration);
+         element.scrollTop = val;
+         if(currentTime < duration) {
+             setTimeout(animateScroll, increment);
+         }
+     };
+     animateScroll();
+ }
+ 
+ //t = current time
+ //b = start value
+ //c = change in value
+ //d = duration
+ Math.easeInOutQuad = function (t, b, c, d) {
+   t /= d/2;
+     if (t < 1) return c/2*t*t + b;
+     t--;
+     return -c/2 * (t*(t-2) - 1) + b;
+ };
 
+ scrollTo('.one', '.six', 1000 )
