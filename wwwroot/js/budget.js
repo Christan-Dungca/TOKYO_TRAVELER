562850680d4bd9dd439b44e93515b0379031c84e
Jordan McKenzie <jordanmckenz@gmail.com>
2019-09-25 21:28:46 -0700

    Fixed logic on budget app. created results area

--- wwwroot/js/budget.js
+++ wwwroot/js/budget.js
@@ -1,17 +1,21 @@
-var slider = document.getElementById("myRange");
-    var slider2 = document.getElementById("myRange2");
-    var output = document.getElementById("demo");
-    var dur = document.getElementById("duration");
-    var start = document.getElementById("amount");
-    var total = document.getElementById("total");
-    var min1 = 0,
-        min2 = 300,
-        min3 = 750,
-        min4 = 1000,
-        max1 = 299,
-        max2 = 749,
-        max3 = 999,
-        max4 = 3000;
+    var slider = document.getElementById("myRange"),
+        output = document.getElementById("demo"),
+        dur = document.getElementById("duration"),
+        start = document.getElementById("amount"),
+        total = document.getElementById("total");
+//variables represent the average spending amount per 
+    var smHousing = 22,
+        medHousing = 45,
+        bigHousing = 75,
+        luxuryHousing = 100,
+        smFood = 5,
+        medFood = 10,
+        bigFood = 25,
+        luxuryFood = 50,
+        smTrans = 10,
+        bigTrans = 20,
+        smLeisure = 10,
+        bigLeisure = 20;
     output.innerHTML = slider.value;
 
     slider.oninput = function() {
@@ -24,57 +28,60 @@ var slider = document.getElementById("myRange");
     //if the slider is under 25 and bigger than 0, set the slider value to something else then do the calc
     //maybe set the result to a returnable variable and use that variable to continue the calc
     function getinfo(){
-        if(slider.value >= 0 && slider.value <=25)
-        {
-            range = 0.25;
-            
-            var result = ((max1 - min1) * (range) + min1);
+
+        if(slider.value == 0){
+            var result = (smHousing + smFood + smTrans + smLeisure) * (dur.value);
         }
-        if(slider.value >= 26 && slider.value <=50)
-        {
-            range = 0.5;
-            var result = ((max1 - min1) * (range) + min1);
+        if(slider.value == 33){
+            var result = (medHousing + medFood + smTrans + smLeisure) * (dur.value);
         }
-        if(slider.value >= 51 && slider.value <=75)
-        {
-            range = 0.75;
-            var result = ((max1 - min1) * (range) + min1);
+        if(slider.value == 66){
+            var result = (bigHousing + bigFood + bigTrans + bigLeisure) * (dur.value);
         }
-        if(slider.value >= 76 && slider.value <=100)
-        {
-            range = 1;
-            var result = ((max1 - min1) * (range) + min1);
-            
+        if(slider.value == 99){
+            var result = (luxuryHousing + luxuryFood + bigTrans + bigLeisure) * (dur.value);
         }
-        /////////////////////////////////////////////////////////////////////////////
-        if(slider2.value >= 0 && slider2.value <=25)
-        {
-            range = 0.25;
-            var result = ((max1 - min1) * (range) + min1);
-        }
-        if(slider2.value >= 26 && slider2.value <=50)
-        {
-            range = 0.5;
-            var result = ((max1 - min1) * (range) + min1);
-        }
-        if(slider2.value >= 51 && slider2.value <=75)
-        {
-            range = 0.75;
-            var result = ((max1 - min1) * (range) + min1);
-        }
-        if(slider2.value >= 76 && slider2.value <=100)
-        {
-            range = 1;
-            var result = ((max1 - min1) * (range) + min1);
-            // document.getElementById("test").innerHTML = result;
-            
-        }
+        
+                    
         console.log(result);
-        //budget is start value - result / amount of days
-        if(result >= 0){
-            document.getElementById("total").innerHTML = Math.floor((start.value - result) / (dur.value))+ " Per Day";
-        }else{
-            result = 0;
-            return(document.getElementById("err").innerHTML = "Invalid Amount, please change your amount");
+        var finalValue = (start.value - result) / (dur.value);
+        var extra = ((start.value) - result * dur.value);
+
+        if(extra >= 0 && slider.value == 0){
+            document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+            document.getElementById("totalHousing").innerHTML = "$" + Math.floor((smHousing) * (dur.value))+ " On Average For Housing Per Day";
+            document.getElementById("totalFood").innerHTML = "$" + Math.floor((smFood) * (dur.value))+ " On Average For Food Per Day";
+            document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((smLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+            document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((smTrans) * (dur.value))+ " On Average for Transportation";
+            document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+        }
+        else if(extra >= 0 && slider.value == 33){
+            document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+            document.getElementById("totalHousing").innerHTML = "$" + Math.floor((medHousing) * (dur.value))+ " On Average For Housing Per Day";
+            document.getElementById("totalFood").innerHTML = "$" + Math.floor((medFood) * (dur.value))+ " On Average For Food Per Day";
+            document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((smLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+            document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((smTrans) * (dur.value))+ " On Average for Transportation";
+            document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+        
+        }
+        else if(extra >= 0 && slider.value == 66){
+            document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+            document.getElementById("totalHousing").innerHTML = "$" + Math.floor((bigHousing) * (dur.value))+ " On Average For Housing Per Day";
+            document.getElementById("totalFood").innerHTML = "$" + Math.floor((bigFood) * (dur.value))+ " On Average For Food Per Day";
+            document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((bigLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+            document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((bigTrans) * (dur.value))+ " On Average for Transportation";
+            document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+        }
+        else if(extra >= 0 && slider.value == 99){
+            document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+            document.getElementById("totalHousing").innerHTML = "$" + Math.floor((luxuryHousing) * (dur.value))+ " On Average For Housing Per Day";
+            document.getElementById("totalFood").innerHTML = "$" + Math.floor((luxuryFood) * (dur.value))+ " On Average For Food Per Day";
+            document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((bigLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+            document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((bigTrans) * (dur.value))+ " On Average for Transportation";
+            document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+        }
+        else{
+            
+            return(document.getElementById("err").innerHTML = "Invalid Amount, Please check your information");
         }
     }
