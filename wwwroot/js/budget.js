8641a9b0dc5434d99f9a95e114453593bfbfaed8
Jordan McKenzie <jordanmckenz@gmail.com>
2019-10-05 21:51:02 -0700

    fixed bug that broke budget app

--- wwwroot/js/budget.js
+++ wwwroot/js/budget.js
@@ -167,58 +167,58 @@ $(document).ready(function(){
     ///////////////////////////////////////////////////////////////////////////////////
     ///////////THIS IS FOR ONE SLIDER//////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////
-    // if(slider.value == 0){
-    //     var result = (smHousing + smFood + smTrans + smLeisure) * (dur.value);
-    // }
-    // if(slider.value == 33){
-    //     var result = (medHousing + medFood + smTrans + smLeisure) * (dur.value);
-    // }
-    // if(slider.value == 66){
-    //     var result = (bigHousing + bigFood + bigTrans + bigLeisure) * (dur.value);
-    // }
-    // if(slider.value == 99){
-    //     var result = (luxuryHousing + luxuryFood + bigTrans + bigLeisure) * (dur.value);
-    // }
+    if(slider.value == 0){
+        var result = (smHousing + smFood + smTrans + smLeisure) * (dur.value);
+    }
+    if(slider.value == 33){
+        var result = (medHousing + medFood + smTrans + smLeisure) * (dur.value);
+    }
+    if(slider.value == 66){
+        var result = (bigHousing + bigFood + bigTrans + bigLeisure) * (dur.value);
+    }
+    if(slider.value == 99){
+        var result = (luxuryHousing + luxuryFood + bigTrans + bigLeisure) * (dur.value);
+    }
     
                 
-    // console.log(result);
-    // var finalValue = (start.value - result) / (dur.value);
-    // var extra = ((start.value) - result * dur.value);
+    console.log(result);
+    var finalValue = (start.value - result) / (dur.value);
+    var extra = ((start.value) - result * dur.value);
 
-    // if(extra >= 0 && slider.value == 0){
-    //     document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
-    //     document.getElementById("totalHousing").innerHTML = "$" + Math.floor((smHousing) * (dur.value))+ " On Average For Housing Per Day";
-    //     document.getElementById("totalFood").innerHTML = "$" + Math.floor((smFood) * (dur.value))+ " On Average For Food Per Day";
-    //     document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((smLeisure) * (dur.value))+ " On Average For Leisure Per Day";
-    //     document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((smTrans) * (dur.value))+ " On Average for Transportation";
-    //     document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
-    // }
-    // else if(extra >= 0 && slider.value == 33){
-    //     document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
-    //     document.getElementById("totalHousing").innerHTML = "$" + Math.floor((medHousing) * (dur.value))+ " On Average For Housing Per Day";
-    //     document.getElementById("totalFood").innerHTML = "$" + Math.floor((medFood) * (dur.value))+ " On Average For Food Per Day";
-    //     document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((smLeisure) * (dur.value))+ " On Average For Leisure Per Day";
-    //     document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((smTrans) * (dur.value))+ " On Average for Transportation";
-    //     document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+    if(extra >= 0 && slider.value == 0){
+        document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+        document.getElementById("totalHousing").innerHTML = "$" + Math.floor((smHousing) * (dur.value))+ " On Average For Housing Per Day";
+        document.getElementById("totalFood").innerHTML = "$" + Math.floor((smFood) * (dur.value))+ " On Average For Food Per Day";
+        document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((smLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+        document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((smTrans) * (dur.value))+ " On Average for Transportation";
+        document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+    }
+    else if(extra >= 0 && slider.value == 33){
+        document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+        document.getElementById("totalHousing").innerHTML = "$" + Math.floor((medHousing) * (dur.value))+ " On Average For Housing Per Day";
+        document.getElementById("totalFood").innerHTML = "$" + Math.floor((medFood) * (dur.value))+ " On Average For Food Per Day";
+        document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((smLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+        document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((smTrans) * (dur.value))+ " On Average for Transportation";
+        document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
     
-    // }
-    // else if(extra >= 0 && slider.value == 66){
-    //     document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
-    //     document.getElementById("totalHousing").innerHTML = "$" + Math.floor((bigHousing) * (dur.value))+ " On Average For Housing Per Day";
-    //     document.getElementById("totalFood").innerHTML = "$" + Math.floor((bigFood) * (dur.value))+ " On Average For Food Per Day";
-    //     document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((bigLeisure) * (dur.value))+ " On Average For Leisure Per Day";
-    //     document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((bigTrans) * (dur.value))+ " On Average for Transportation";
-    //     document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
-    // }
-    // else if(extra >= 0 && slider.value == 99){
-    //     document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
-    //     document.getElementById("totalHousing").innerHTML = "$" + Math.floor((luxuryHousing) * (dur.value))+ " On Average For Housing Per Day";
-    //     document.getElementById("totalFood").innerHTML = "$" + Math.floor((luxuryFood) * (dur.value))+ " On Average For Food Per Day";
-    //     document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((bigLeisure) * (dur.value))+ " On Average For Leisure Per Day";
-    //     document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((bigTrans) * (dur.value))+ " On Average for Transportation";
-    //     document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
-    // }
-    // else{
+    }
+    else if(extra >= 0 && slider.value == 66){
+        document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+        document.getElementById("totalHousing").innerHTML = "$" + Math.floor((bigHousing) * (dur.value))+ " On Average For Housing Per Day";
+        document.getElementById("totalFood").innerHTML = "$" + Math.floor((bigFood) * (dur.value))+ " On Average For Food Per Day";
+        document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((bigLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+        document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((bigTrans) * (dur.value))+ " On Average for Transportation";
+        document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+    }
+    else if(extra >= 0 && slider.value == 99){
+        document.getElementById("total").innerHTML = "$" + Math.floor(result)+ " About per day ";
+        document.getElementById("totalHousing").innerHTML = "$" + Math.floor((luxuryHousing) * (dur.value))+ " On Average For Housing Per Day";
+        document.getElementById("totalFood").innerHTML = "$" + Math.floor((luxuryFood) * (dur.value))+ " On Average For Food Per Day";
+        document.getElementById("totalLeisure").innerHTML = "$" + Math.floor((bigLeisure) * (dur.value))+ " On Average For Leisure Per Day";
+        document.getElementById("totalTransportation").innerHTML = "$" + Math.floor((bigTrans) * (dur.value))+ " On Average for Transportation";
+        document.getElementById("extra").innerHTML = "$" + Math.floor(extra)+ " Extra";
+    }
+    else{
         
-    //     return(document.getElementById("err").innerHTML = "Invalid Amount, Please check your information");
-    // }
+        return(document.getElementById("err").innerHTML = "Invalid Amount, Please check your information");
+    }
